a435e172e4ca055da590f4a86bcbd40b8e7ab38b
Daisuke Takahashi <Daisuke.Takahashi@jp.sony.com>
2017-01-07 21:47:15 +0900

    hello world, from rust

--- src/lib.rs
+++ src/lib.rs
@@ -9,9 +9,18 @@ extern crate rlibc; //import baremetal rawr api in rust
 // put extern to be compartible with C lang 
 #[no_mangle] 
 pub extern fn rust_main() {
-    let x = ["Hello", "World", "!"];
-    let y = x;
-    let test = (0..3).flat_map(|x| 0..x).zip(0..);
+    let hello = b"Hello World!"; //define byte string
+    let color_byte = 0x1f;
+    
+    let mut hello_colored = [color_byte; 24];
+    for (i, char_byte) in hello.into_iter().enumerate() {
+        hello_colored[i*2] = *char_byte;
+    }
+
+    let buffer_ptr = (0xb8000 + 1988) as *mut _;
+    unsafe { *buffer_ptr = hello_colored }; //tell rust that this ptr operation is unsafe
+
+    loop{}
 } 
 //lang defines language item: plugin to the compiler
 #[lang = "eh_personality"] extern fn eh_personality() {} // define action in response to Rust's panic!.
