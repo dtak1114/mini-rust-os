2d6ab3e24f5bb50528a0fea1e1d85883c1959634
Daisuke Takahashi <Daisuke.Takahashi@jp.sony.com>
2017-01-07 20:32:03 +0900

    added no mangle option to main

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,7 @@
 // defining attributes
 #![feature(lang_items)] // allow lang_items feature attribute.(only in nightly build0
 #![no_std] // do not auto link std library. we cannot use!
-
-pub extern fn rust_main() {} // put extern to be compartible with C lang 
+#[no_mangle] pub extern fn rust_main() {} // put extern to be compartible with C lang 
 
 //lang defines language item: plugin to the compiler
 #[lang = "eh_personality"] extern fn eh_personality() {} // define action in response to Rust's panic!.
