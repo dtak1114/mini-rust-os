5a5665d17080b3d9754449e8cfe938a5cdb997d3
Daisuke Takahashi <Daisuke.Takahashi@jp.sony.com>
2017-01-09 18:45:58 +0900

    overwrite println macro to use our buffer

--- src/lib.rs
+++ src/lib.rs
@@ -10,25 +10,21 @@
 extern crate rlibc; //import baremetal rawr api in rust
 extern crate volatile; //keep buffer out of optimization
 extern crate spin; //introduce spinlock to control race condition for out static writer
+
+#[macro_use]
 mod vga_buffer;
 
 // put extern to be compartible with C lang 
 #[no_mangle] 
 pub extern fn rust_main() {
-    // let hello = b"Hello World!"; //define byte string
-    // let color_byte = 0x1f;
-    
-    // let mut hello_colored = [color_byte; 24]; // create array size of 24  with color_byte
-    // for (i, char_byte) in hello.into_iter().enumerate() {
-        // hello_colored[i*2] = *char_byte; //create color=bye + char_byte pair for each character
-    // }
+    vga_buffer::clear_screen();
+    println!("Hello, {}!", "World");
 
-    // let buffer_ptr = (0xb8000 + 1988) as *mut _;
-    // unsafe { *buffer_ptr = hello_colored }; //tell rust that this ptr operation is unsafe
-    vga_buffer::WRITER.lock().write_str("Hello again");
+    // use core::fmt::Write;
+    // vga_buffer::WRITER.lock().write_str("Hello again");
 
-    use core::fmt::Write; // required import to use write! macro 
-    write!(vga_buffer::WRITER.lock(), ", some numbers: {} {}", 42, 1.337);
+    // // use core::fmt::Write; // required import to use write! macro 
+    // write!(vga_buffer::WRITER.lock(), ", some numbers: {} {}", 42, 1.337);
     loop{}
 } 
 //lang defines language item: plugin to the compiler
--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -87,14 +87,14 @@ impl Writer {
     fn new_line(&mut self){ 
         //move all current character 1 line upper row
         for row in 1..BUFFER_HEIGHT {
-            for col in 1..BUFFER_WIDTH {
+            for col in 0..BUFFER_WIDTH {
                 let buffer = self.buffer();
                 let character = buffer.chars[row][col].read();
                 buffer.chars[row-1][col].write(character);
             }
         }
         //remove old row
-        self.clear_row(BUFFER_HEIGHT);
+        self.clear_row(BUFFER_HEIGHT-1);
         self.column_position = 0;
     }
 
@@ -128,17 +128,39 @@ pub static WRITER: Mutex<Writer> = Mutex::new(Writer{
     column_position: 0,
     color_code: ColorCode::new(Color::LightGreen, Color::Black),
     buffer: unsafe { Unique::new(0xb8000 as *mut _ )}
-    });
+});
 
-pub fn print_something() {
-    use core::fmt::Write;
-    let mut writer = Writer {
-        column_position: 0,
-        color_code: ColorCode::new(Color::LightGreen, Color::Black),
-        buffer: unsafe { Unique::new(0xb8000 as *mut _) }
-    };
 
-    writer.write_byte(b'H');
-    writer.write_str("ello! ");
-    write!(writer, "The numbers are {} and {}", 42, 1.0/3.0);
+//just copy
+macro_rules! println {
+    ($fmt:expr) => (print!(concat!($fmt, "\n")));
+    ($fmt:expr, $($arg:tt)*) => (print!(concat!($fmt, "\n"), $($arg)*));
 }
+
+//wrap standard print to only fit to our VGA adaptor
+macro_rules! print {
+    ($($arg:tt)*) => ({
+        use core::fmt::Write;
+        let mut writer = $crate::vga_buffer::WRITER.lock();
+        writer.write_fmt(format_args!($($arg)*)).unwrap();
+    });
+}
+
+pub fn clear_screen() {
+    for _ in 0..BUFFER_HEIGHT {
+        println!("");
+    }
+}
+
+// pub fn print_something() {
+    // use core::fmt::Write;
+    // let mut writer = Writer {
+        // column_position: 0,
+        // color_code: ColorCode::new(Color::LightGreen, Color::Black),
+        // buffer: unsafe { Unique::new(0xb8000 as *mut _) }
+    // };
+
+    // writer.write_byte(b'H');
+    // writer.write_str("ello! ");
+    // write!(writer, "The numbers are {} and {}", 42, 1.0/3.0);
+// }
